107755951253c58fc763b97d68fd2f048244868a
parkmineum <a01087164428@gmail.com>
2025-01-03 17:04:00 +0900

    Initial Settings

---
+++ src/test/java/com/gdg/z_meet/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.z_meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
