976296034280297edcbeda5f9db4cc5355b7064a
parkmineum <a01087164428@gmail.com>
2025-01-03 23:18:53 +0900

    [chore] Swagger 설정

---
+++ src/main/java/com/gdg/z_meet/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.gdg.z_meet.global.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("GDG Z밋 Swagger")
+                .description("GDG Z밋 Swagger 입니다.")
+                .version("1.0.0");
+    }
+}
