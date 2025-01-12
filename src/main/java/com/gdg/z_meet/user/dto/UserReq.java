7d09e4fc86f791da16215eb944dacf1f1f3e7bf9
YWSIK <yuusik818@gmail.com>
2025-01-13 04:06:40 +0900

    #1 [feat] dto 추가

--- src/main/java/com/gdg/z_meet/user/dto/UserReq.java
+++ src/main/java/com/gdg/z_meet/user/dto/UserReq.java
@@ -1,4 +1,38 @@
 package com.gdg.z_meet.user.dto;
 
+import com.gdg.z_meet.user.entity.profile.*;
+import lombok.*;
+
 public class UserReq {
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class LoginReq{
+        private String studentNumber;
+        private String password;
+    }
+
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class SignUpReq{
+        private String name;
+        private String studentNumber;
+        private String password;
+        private Grade grade;
+        private int age;
+        private Gender gender;
+        private Major major;
+        private String nickname;
+        private Emoji emoji;
+        private MBTI mbti;
+        private Music music;
+        private Style style;
+        private IdealAge idealAge;
+        private IdealType idealType;
+    }
 }
--- src/main/java/com/gdg/z_meet/user/dto/UserRes.java
+++ src/main/java/com/gdg/z_meet/user/dto/UserRes.java
@@ -1,4 +1,14 @@
 package com.gdg.z_meet.user.dto;
 
+import lombok.*;
+
 public class UserRes {
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class SignUpRes {
+        private String message;
+    }
 }
