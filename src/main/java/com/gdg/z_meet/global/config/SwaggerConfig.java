0cd6455ab9dbdc2bf6de25bae36a6cbe675b2509
seoyeoneel02 <seoyeoneel02@gmail.com>
2025-01-23 14:05:21 +0900

    feat: 스웨거 토큰 인증 구현 #19

--- src/main/java/com/gdg/z_meet/global/config/SwaggerConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.gdg.z_meet.global.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +13,30 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        String accessTokenSchemeName = "accessToken";
+        String refreshTokenSchemeName = "refreshToken";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(accessTokenSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(accessTokenSchemeName, new SecurityScheme()
+                        .name("accessToken")
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+                .addSecuritySchemes(refreshTokenSchemeName, new SecurityScheme()
+                        .name("refreshToken")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement);
     }
 
     private Info apiInfo() {
