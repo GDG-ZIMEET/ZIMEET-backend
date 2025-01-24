323f15b511e5afe93289ec17fc3170e13ff157f0
parkmineum <a01087164428@gmail.com>
2025-01-24 18:07:06 +0900

    [chore] Payment FeignClient 관련 세팅

--- src/main/java/com/gdg/z_meet/Application.java
+++ src/main/java/com/gdg/z_meet/Application.java
@@ -3,12 +3,14 @@ package com.gdg.z_meet;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class Application {
 
 	@PostConstruct
