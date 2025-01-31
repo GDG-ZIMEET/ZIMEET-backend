be904970bf5631aacd0848e7841ec9343d7d0659
seoyeoneel02 <seoyeoneel02@gmail.com>
2025-01-31 18:43:15 +0900

    docs: 스웨거 명세 추가 #19

--- src/main/java/com/gdg/z_meet/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/gdg/z_meet/domain/meeting/controller/MeetingController.java
@@ -8,6 +8,7 @@ import com.gdg.z_meet.domain.user.entity.User;
 import com.gdg.z_meet.global.common.AuthenticatedUserUtils;
 import com.gdg.z_meet.global.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +18,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/meeting")
+@Tag(name = "Meeting")
 @Validated
 public class MeetingController {
 
     private final MeetingQueryService meetingQueryService;
 
-    @Operation(summary = "팀 상세 조회")
-    @GetMapping("/detail/{teamId}")
+    @Operation(summary = "팀 소개 상세 조회", description = "본인 팀은 조회가 불가능합니다.")
+    @GetMapping("/{teamId}")
     public Response<MeetingResponseDTO.GetTeamDTO> getTeam(@PathVariable Long teamId) {
 
         Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
