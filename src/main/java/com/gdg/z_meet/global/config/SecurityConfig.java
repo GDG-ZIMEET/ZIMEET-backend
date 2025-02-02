d87ce471878d403797f7a751f6df4459b2de6487
parkmineum <a01087164428@gmail.com>
2025-02-03 03:07:58 +0900

    [fix] CORS 설정 수정

--- src/main/java/com/gdg/z_meet/global/config/SecurityConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,9 +17,81 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    private final JwtUtil jwtUtil;
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+////                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안 함
+//                )
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/jwt/**", "/swagger-ui/**", "/v3/api-docs/**", "/booths/**").permitAll() // 인증 없이 접근 허용
+//                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+//                )
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+//        configuration.setAllowedOrigins(List.of("localhost:3000", "localhost:8080", "http://52.79.78.62:8081"));
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+//        configuration.setAllowCredentials(true);  // 쿠키 및 인증 정보 허용
+//
+//        configuration.addExposedHeader("Access-Control-Allow-Origin");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilter() {
+//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // 필터 우선순위를 가장 높게 설정
+//        return bean;
+//    }
+//
+//    @RequiredArgsConstructor
+//    @Configuration
+//    public class WebConfig implements WebMvcConfigurer {
+//
+//        @Bean
+//        public WebMvcConfigurer webMvcConfigurer() {
+//            return new WebMvcConfigurer() {
+//
+//                public void addCorsMappings(CorsRegistry registry) {
+//                    registry.addMapping("/**")
+//                            .allowedOrigins("*")
+//                            .allowedMethods(
+//                                    HttpMethod.GET.name(),
+//                                    HttpMethod.HEAD.name(),
+//                                    HttpMethod.POST.name(),
+//                                    HttpMethod.PUT.name(),
+//                                    HttpMethod.DELETE.name());
+//                }
+//            };
+//        }
+//    }
+//}
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -29,7 +102,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(withDefaults()) // WebMvcConfigurer 에서 설정한 CORS 적용
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안 함
                 )
@@ -42,26 +115,27 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+    @RequiredArgsConstructor
+    @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
 
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
-        configuration.setAllowedOrigins(List.of("localhost:3000", "localhost:8080", "http://52.79.78.62:8081"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowCredentials(true);  // 쿠키 및 인증 정보 허용
+        @Bean
+        public WebMvcConfigurer webMvcConfigurer() {
+            return new WebMvcConfigurer() {
 
-        configuration.addExposedHeader("Access-Control-Allow-Origin");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // 필터 우선순위를 가장 높게 설정
-        return bean;
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*") // 모든 Origin 허용
+                            .allowedMethods(
+                                    HttpMethod.GET.name(),
+                                    HttpMethod.HEAD.name(),
+                                    HttpMethod.POST.name(),
+                                    HttpMethod.PUT.name(),
+                                    HttpMethod.DELETE.name());
+                }
+            };
+        }
     }
 }
+
