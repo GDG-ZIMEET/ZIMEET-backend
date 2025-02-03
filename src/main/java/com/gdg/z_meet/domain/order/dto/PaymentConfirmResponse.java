5ea52ebd7e61d24d541b57a01a6e26b4961cadfd
parkmineum <a01087164428@gmail.com>
2025-02-03 21:31:06 +0900

    [feat] TossPayments 변경 사항

--- src/main/java/com/gdg/z_meet/domain/order/dto/PaymentConfirmResponse.java
+++
@@ -1,4 +0,0 @@
-package com.gdg.z_meet.domain.order.dto;
-
-public class PaymentConfirmResponse {
-}
--- src/main/java/com/gdg/z_meet/domain/order/infrastructure/RequestInterceptor.java
+++
@@ -1,9 +0,0 @@
-package com.gdg.z_meet.domain.order.infrastructure;
-
-import feign.RequestTemplate;
-
-@FunctionalInterface
-public interface RequestInterceptor {
-    void apply(RequestTemplate template);     // OpenFeign
-
-}
