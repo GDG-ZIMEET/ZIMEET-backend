0547a1b1d189e849b8e5309bf9f3764d781dea63
Seoyeon <seoyeoneel02@gmail.com>
2025-02-19 03:19:58 +0900

    feat: 티켓 개수 조회 #70

--- src/main/java/com/gdg/z_meet/domain/meeting/controller/RandomController.java
+++ src/main/java/com/gdg/z_meet/domain/meeting/controller/RandomController.java
@@ -1,17 +1,11 @@
 package com.gdg.z_meet.domain.meeting.controller;
 
-import com.gdg.z_meet.domain.meeting.dto.MeetingRequestDTO;
-import com.gdg.z_meet.domain.meeting.dto.MeetingResponseDTO;
 import com.gdg.z_meet.domain.meeting.dto.RandomResponseDTO;
-import com.gdg.z_meet.domain.meeting.entity.TeamType;
-import com.gdg.z_meet.domain.meeting.service.MeetingCommandService;
-import com.gdg.z_meet.domain.meeting.service.MeetingQueryService;
+import com.gdg.z_meet.domain.meeting.service.RandomQueryService;
 import com.gdg.z_meet.global.common.AuthenticatedUserUtils;
 import com.gdg.z_meet.global.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,15 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class RandomController {
 
-    private final MeetingQueryService meetingQueryService;
-    private final MeetingCommandService meetingCommandService;
+    private final RandomQueryService randomQueryService;
 
     @Operation(summary = "남은 티켓 개수")
     @GetMapping("/ticket")
-    public Response<RandomResponseDTO.GetTicketDTO> getMyDelete() {
+    public Response<RandomResponseDTO.GetTicketDTO> getTicket() {
 
         Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
-        RandomResponseDTO.GetTicketDTO response;
+        RandomResponseDTO.GetTicketDTO response = randomQueryService.getTicket(userId);
 
         return Response.ok(response);
     }
