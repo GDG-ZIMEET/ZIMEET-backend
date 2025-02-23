283c545e23fdff0e7ba797c4be390f0f69580772
haeun <haeun9634@naver.com>
2025-02-23 20:41:22 +0900

    Refactor: 사용자 Id JWT가 아닌 Security 사용하도록 수정

--- src/main/java/com/gdg/z_meet/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/gdg/z_meet/domain/chat/controller/ChatRoomController.java
@@ -6,6 +6,7 @@ import com.gdg.z_meet.domain.chat.service.ChatRoomCommandService;
 import com.gdg.z_meet.domain.chat.service.ChatRoomQueryService;
 import com.gdg.z_meet.domain.chat.service.MessageService;
 import com.gdg.z_meet.domain.meeting.dto.MeetingRequestDTO;
+import com.gdg.z_meet.global.common.AuthenticatedUserUtils;
 import com.gdg.z_meet.global.jwt.JwtUtil;
 import com.gdg.z_meet.global.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
@@ -52,11 +53,9 @@ public class ChatRoomController {
     @Operation(summary = "사용자 채팅방 제거", description = "사용자를 지정된 채팅방에서 제거합니다. 채팅방 나가기와 동일한 기능 입니다. ")
     @DeleteMapping("/{roomId}/users")
     public Response<String> removeUserFromChatRoom(
-            @RequestHeader("Authorization") String token,
             @PathVariable Long roomId) {
 
-        // 토큰에서 사용자 ID 추출
-        Long userId = jwtUtil.extractUserIdFromToken(token);
+        Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
 
         // 채팅방에서 사용자 제거
         chatRoomCommandService.removeUserFromChatRoom(roomId, userId);
@@ -66,9 +65,8 @@ public class ChatRoomController {
 
     @Operation(summary = "사용자 참여 채팅방 조회", description = "사용자가 현재 참여 중인 채팅방 목록을 조회합니다.")
     @GetMapping("/users")
-    public Response<List<ChatRoomDto.chatRoomListDto>> getUserChatRooms(
-            @RequestHeader("Authorization") String token) {
-        Long userId = jwtUtil.extractUserIdFromToken(token); // JWT 토큰에서 사용자 ID 추출
+    public Response<List<ChatRoomDto.chatRoomListDto>> getUserChatRooms() {
+        Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
         List<ChatRoomDto.chatRoomListDto> chatRooms = chatRoomQueryService.getChatRoomsByUser(userId); // 참여 중인 채팅방 조회
         return Response.ok(chatRooms); // 채팅방 목록 반환
     }
@@ -76,22 +74,19 @@ public class ChatRoomController {
     @Operation(summary = "채팅방 사용자 조회 ", description = "특정 채팅방에 있는 사용자들을 조회합니다. ")
     @GetMapping("/{roomId}")
     public Response<List<ChatRoomDto.chatRoomUserList>> sendMessage(
-            @RequestHeader("Authorization") String token,
             @PathVariable Long roomId) {
-        Long userId = jwtUtil.extractUserIdFromToken(token); // JWT 토큰에서 사용자 ID 추출
-
+        Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
         return Response.ok(chatRoomQueryService.getUserByRoomId(userId, roomId)); // 성공 응답 반환
     }
 
     @Operation(summary = "메시지 조회", description = "지정된 채팅방의 메시지를 페이지네이션을 사용하여 조회합니다.")
     @GetMapping("/{roomId}/messages")
     public ResponseEntity<List<ChatMessage>> getMessages(
-            @RequestHeader("Authorization") String token,
             @PathVariable Long roomId,
             @RequestParam(defaultValue = "0") int page, // 페이지 번호 (기본값: 0)
             @RequestParam(defaultValue = "15") int size // 페이지 크기 (기본값: 20)
     ) {
-        Long userId = jwtUtil.extractUserIdFromToken(token);
+        Long userId = AuthenticatedUserUtils.getAuthenticatedUserId();
         List<ChatMessage> messages = messageService.getMessagesByChatRoom(roomId,userId, page, size);
         return ResponseEntity.ok(messages);
     }
