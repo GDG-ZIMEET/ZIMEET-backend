6c563243bfa1570da3e452f513d4b422f1d47e69
parkmineum <a01087164428@gmail.com>
2025-03-14 14:26:13 +0900

    [chore] 인증 필터에 서버 헬스 체킹 제외

--- src/main/java/com/gdg/z_meet/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/gdg/z_meet/global/jwt/JwtAuthenticationFilter.java
@@ -28,6 +28,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/api/health") | path.startsWith("/swagger/*") | path.startsWith("/swagger-ui/*");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String uri = request.getRequestURI();
