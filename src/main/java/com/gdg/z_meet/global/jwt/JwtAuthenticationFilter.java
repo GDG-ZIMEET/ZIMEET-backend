5f2d3e5c204bc0f90000dcd3544cb7ba811112df
parkmineum <a01087164428@gmail.com>
2025-03-19 02:38:18 +0900

    [chore] 시큐리티 필터 제외

--- src/main/java/com/gdg/z_meet/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/gdg/z_meet/global/jwt/JwtAuthenticationFilter.java
@@ -45,8 +45,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 || path.startsWith("/swagger") || path.startsWith("/swagger-ui") || path.startsWith("/v3/api-docs")
                 || path.startsWith("/resources/static/")
                 || path.startsWith("/api/booths") || path.startsWith("/api/event")
-                || path.startsWith("/api/user")
-                || path.startsWith("/ws") || path.startsWith("/ws/info");
+                || path.startsWith("/api/user");
+//                || path.startsWith("/ws") || path.startsWith("/ws/info");
     }
 
     @Override
