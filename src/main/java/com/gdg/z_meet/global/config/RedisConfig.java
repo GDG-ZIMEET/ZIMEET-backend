16986f4c0e18dbdbd4616072e22dcd5e749e392b
parkmineum <a01087164428@gmail.com>
2025-03-19 04:57:46 +0900

    [fix] Bean 객체 직렬화 오류 수정

--- src/main/java/com/gdg/z_meet/global/config/RedisConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.gdg.z_meet.global.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,14 +40,24 @@ public class RedisConfig {
         // Jackson 설정: 타입 정보 추가
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new Hibernate5Module());
+
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);      // 빈 객체 직렬화 오류 방지
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
 
         // 타입 정보 활성화 (대체 방법)
         objectMapper.activateDefaultTyping(
                 objectMapper.getPolymorphicTypeValidator(),
-                ObjectMapper.DefaultTyping.EVERYTHING,
+                ObjectMapper.DefaultTyping.NON_FINAL,
                 JsonTypeInfo.As.PROPERTY
         );
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashValueSerializer(serializer);
 
         // Key와 HashKey 직렬화
         redisTemplate.setKeySerializer(new StringRedisSerializer());
