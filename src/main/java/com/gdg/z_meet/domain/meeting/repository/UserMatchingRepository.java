5ad1760d4ded9a3c6e87b40e205fcf3a12e8191e
parkmineum <a01087164428@gmail.com>
2025-03-20 00:45:23 +0900

    [chore] 매칭 조인 시 동시성 제어 추가

--- src/main/java/com/gdg/z_meet/domain/meeting/repository/UserMatchingRepository.java
+++ src/main/java/com/gdg/z_meet/domain/meeting/repository/UserMatchingRepository.java
@@ -1,15 +1,17 @@
 package com.gdg.z_meet.domain.meeting.repository;
 
 import com.gdg.z_meet.domain.meeting.entity.UserMatching;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserMatchingRepository extends JpaRepository<UserMatching, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT um FROM UserMatching um JOIN FETCH um.user u JOIN FETCH u.userProfile WHERE um.matching.id = :matchingId")
     List<UserMatching> findAllByMatchingIdWithUserProfile(@Param("matchingId") Long matchingId);
 
