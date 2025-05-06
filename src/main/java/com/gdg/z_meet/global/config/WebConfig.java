ed3b577db571006b79c2a75a0a98839efc3a38c1
parkmineum <a01087164428@gmail.com>
2025-05-06 21:38:39 +0900

    [feat] 인증된 사용자 정보 자동 주입 되도록 커스텀

--- src/main/java/com/gdg/z_meet/global/config/WebConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/WebConfig.java
@@ -1,14 +1,31 @@
 package com.gdg.z_meet.global.config;
 
+import com.gdg.z_meet.global.jwt.AuthUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import java.util.List;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver userIdArgumentResolver;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/swagger", "/swagger-ui/index.html");
         registry.addRedirectViewController("/swagger/", "/swagger-ui/index.html");
+    }
+
+    public WebConfig(AuthUserArgumentResolver userIdArgumentResolver) {
+        this.userIdArgumentResolver = userIdArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
     }
 }
