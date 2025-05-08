8d2d864c735ae7e5f05c127e4868d46fa67f4ae0
parkmineum <a01087164428@gmail.com>
2025-05-08 15:18:34 +0900

    [chore] FCMInitializer 세팅

---
+++ src/main/resources/firebase/FCMInitializer.java
@@ -0,0 +1,41 @@
+package firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FCMInitializer {
+
+    @Value("${firebase.admin-sdk}")
+    private String serviceAccountFile;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            InputStream serviceAccount =
+                    new ClassPathResource(serviceAccountFile).getInputStream();
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl(databaseUrl)
+                    .build();
+
+            // 중복 초기화 방지
+            if (FirebaseApp.getApps().isEmpty()) {
+                System.out.println("Firebase 초기화");
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
