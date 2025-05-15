3c0c29ef8c278159795a44082556561cf45ff56a
parkmineum <a01087164428@gmail.com>
2025-05-16 00:58:34 +0900

    [CHORE] DB Migration 추가

--- src/main/resources/db/migration/V15__Update_Set.sql
+++ src/main/resources/db/migration/V15__Update_Set.sql
@@ -1,4 +1,5 @@
 
 ALTER TABLE fcm_token
     DROP FOREIGN KEY FKe0a0mkcsnkfse7c7pljt8kbn3,
-    ADD CONSTRAINT UC_user_user_id UNIQUE (user_user_id);
+    ADD CONSTRAINT UC_user_user_id UNIQUE (user_user_id),
+    MODIFY token VARCHAR(255) NOT NULL;
