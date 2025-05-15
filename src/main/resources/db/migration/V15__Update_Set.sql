a553e862d3c88fb72411c843539df99a7704d8f5
parkmineum <a01087164428@gmail.com>
2025-05-16 03:09:25 +0900

    [FIX] Migration 오류 해결

--- src/main/resources/db/migration/V15__Update_Set.sql
+++ src/main/resources/db/migration/V15__Update_Set.sql
@@ -1,5 +1,6 @@
-
 ALTER TABLE fcm_token
-    DROP FOREIGN KEY FKe0a0mkcsnkfse7c7pljt8kbn3,
-    ADD CONSTRAINT UC_user_user_id UNIQUE (user_user_id),
+DROP FOREIGN KEY FKe0a0mkcsnkfse7c7pljt8kbn3,
+    CHANGE COLUMN user_user_id user_id BIGINT NOT NULL,
+    ADD CONSTRAINT UC_user_id UNIQUE (user_id),
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(user_id),
     MODIFY token VARCHAR(255) NOT NULL;
